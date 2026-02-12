198e8ec23a20edca164c899c94286302c2a1ea8f
Bhavesh <bam@narola.email>
2026-02-12 19:29:00 +0530

    missing tailwing config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
-const config = {
+// postcss.config.js or postcss.config.mjs
+export default {
   plugins: {
-    "@tailwindcss/postcss": {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
-
-export default config;
