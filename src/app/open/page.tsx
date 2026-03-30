483cdf98ef5cf6e036d20b655e5593a44b73d118
Bhavesh <bam@narola.email>
2026-03-30 17:01:49 +0530

    added route to support deeplinking from mobile apps

---
+++ src/app/open/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+export default function OpenDeepLinkHandler() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const type = searchParams?.get("donrifa_type");
+    const id = searchParams?.get("donrifa_id");
+    const parentId = searchParams?.get("donrifa_parent_id");
+
+    if (!type || !id) {
+      router.replace("/");
+      return;
+    }
+
+    switch (type) {
+      case "raffle":
+        // Navigate to raffle detail page
+        router.replace(`/raffles/${id}?pid=${id}`);
+        break;
+
+      case "invite":
+        router.replace(`/invite/${id}`);
+        break;
+
+      case "merchandise":
+        router.replace(`/product/${id}?parentId=${parentId}`);
+        break;
+
+      default:
+        router.replace("/");
+    }
+  }, [searchParams, router]);
+
+  return null;
+}
