3a6cfbb1d9394d0b2a7884c06ed3121168f214d7
Bhavesh <bam@narola.email>
2026-04-10 19:15:34 +0530

    added events

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics, isSupported, logEvent  } from "firebase/analytics";
+import { getAnalytics, isSupported, logEvent } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAX-vu54c0lsNthzyjm64V-MqHniFuVu60",
-  authDomain: "don-rifa.firebaseapp.com",
-  databaseURL: "https://don-rifa.firebaseio.com",
-  projectId: "don-rifa",
-  storageBucket: "don-rifa.appspot.com",
-  messagingSenderId: "372163631723",
-  appId: "1:372163631723:web:0bc7a871a05744e8f6af2e",
-  measurementId: "G-YRFMY9GL2H"
+    apiKey: "AIzaSyAX-vu54c0lsNthzyjm64V-MqHniFuVu60",
+    authDomain: "don-rifa.firebaseapp.com",
+    databaseURL: "https://don-rifa.firebaseio.com",
+    projectId: "don-rifa",
+    storageBucket: "don-rifa.appspot.com",
+    messagingSenderId: "372163631723",
+    appId: "1:372163631723:web:0bc7a871a05744e8f6af2e",
+    measurementId: "G-YRFMY9GL2H"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,18 +17,25 @@ const app = initializeApp(firebaseConfig);
 let analytics: any = null;
 
 export const initAnalytics = async () => {
-  if (typeof window !== "undefined") {
-    const supported = await isSupported();
-    if (supported) {
-      analytics = getAnalytics(app);
+    if (typeof window !== "undefined") {
+        const supported = await isSupported();
+        if (supported) {
+            console.log("✅ Firebase Analytics Initialized");
+            analytics = getAnalytics(app);
+        }
     }
-  }
 };
 
-export const trackFirebaseEvent = (eventName: string, params?: any) => {
-  if (analytics) {
-    console.log(analytics);
-    
-    logEvent(analytics, eventName, params);
-  }
+export const trackFirebaseEvent = async (eventName: string, params?: any) => {
+    if (!analytics) {
+        const supported = await isSupported();
+        if (supported) {
+            analytics = getAnalytics(app);
+        }
+    }
+
+    if (analytics) {
+        console.log("🔥 Firebase Event:", eventName);
+        logEvent(analytics, eventName, params);
+    }
 };
