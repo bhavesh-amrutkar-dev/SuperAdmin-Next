ce070d0f321a384bc20a9c460cd658e17c24d2fc
Bhavesh <bam@narola.email>
2026-04-16 12:51:16 +0530

    fix the deployemnt related issue

--- src/app/secure-checkout/page.tsx
+++ src/app/secure-checkout/page.tsx
@@ -439,6 +439,7 @@ export default function SecureCheckoutPage() {
       setPlacingOrder(false);
       setIsUpdatingStatus(true);
 
+      trackEvent("ORDER_API_STATUES");
       await fetch("/api/orders/status-update", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -495,6 +496,8 @@ export default function SecureCheckoutPage() {
 
     // 2️⃣ Background backend update (non-blocking)
     setIsUpdatingStatus(true);
+
+    trackEvent("ORDER_API_STATUES");
     await fetch("/api/orders/status-update", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -565,6 +568,7 @@ export default function SecureCheckoutPage() {
       };
       // 1️⃣ Create order (your existing logic)
       trackEvent("GOTO_PAYEMNT");
+
       const response = await fetch("/api/orders/place", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -926,6 +930,7 @@ export default function SecureCheckoutPage() {
         const orderId = localStorage.getItem("orderId");
 
         if (orderId) {
+          trackEvent("ORDER_API_STATUES");
           await fetch("/api/orders/status-update", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
