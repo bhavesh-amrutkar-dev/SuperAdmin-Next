3a592b3ced31d1bff069dcf03fcf41be6c6e45ab
Bhavesh <bam@narola.email>
2026-05-01 11:32:44 +0530

    done place holder chnage

--- src/app/guest-checkout/page.tsx
+++ src/app/guest-checkout/page.tsx
@@ -1126,7 +1126,7 @@ border text-sm transition-all cursor-pointer gap-1
                     setAthMobile(value);
                   }
                 }}
-                placeholder="787 123 4567"
+                placeholder="Enter the mobile number which is registered with ath movil"
                 inputMode="tel"
                 maxLength={10}
               />
